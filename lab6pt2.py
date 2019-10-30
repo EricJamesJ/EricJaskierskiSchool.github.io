3d0bb92b7491b58595ee5d1382487796fa50a669
EricJaskierskiSchool <47194298+EricJaskierskiSchool@users.noreply.github.com>
2019-10-30 18:01:09 -0400

    Add files via upload

---
+++ lab6pt2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+print "Content-type:text/html\n\n"
+print '<html><body>'
+print '<div style="fontsize:3.0em;color:#cc0000;">This is
+my first Python program</div>'
+print '</body></html>'
---
+++ lab6pt2.rb
@@ -0,0 +1,3 @@
+#!/usr/local/bin/ruby -w
+print "Content-type: text/html\n\n"
+print "Hello World!\n"
